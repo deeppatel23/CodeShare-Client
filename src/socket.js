82c5b712ff0d6c1984772fc47f1e14228811719f
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-06-25 12:26:53 +0530

    updated server url

--- src/socket.js
+++ src/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io('http://localhost:5004', options);
+    return io('https://code-share-server.onrender.com', options);
 };
